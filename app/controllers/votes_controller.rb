d3f7d17c9d0862f965883203aff188ff84b26c75
ngjbbs <nickg@journalbooks.com>
2013-06-11 14:02:03 -0400

    FIN

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,11 @@
+class VotesController < ApplicationController
+
+	def create
+	
+@vote = current_user.votes.where(:link_id => params[:vote][:link_id]).first
+@vote ||= current_user.votes.create(params[:vote])
+@vote.update_attributes(:up => params[:vote][:up])
+redirect_to :back
+	end
+
+end
