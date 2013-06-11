3fddde570056bfb440ebfb68ad4dc5149b580101
ngjbbs <nickg@journalbooks.com>
2013-06-11 14:01:55 -0400

    FIN

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,8 @@
 class Vote < ActiveRecord::Base
   belongs_to :link
   belongs_to :user
+	validates :user_id, :uniqueness => { :scope => :link_id }
+
   # attr_accessible :title, :body
   attr_accessible 	:user_id, :link_id, :up
       
